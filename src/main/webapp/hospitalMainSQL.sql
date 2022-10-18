0f647bed14f809edc56c2a753e76df24d8004c21
shj2080 <shj2080@gmail.com>
2022-10-18 16:25:40 +0900

    SQL update

--- src/main/webapp/hospitalMainSQL.sql
+++ src/main/webapp/hospitalMainSQL.sql
@@ -33,7 +33,8 @@ select * from speciality;
 --의사 코드,의사 이름, 진료과 코드
 create table doctor(
    doctor_code int auto_increment primary key, /* 의사 코드 */
-   doctor_name varchar(20) 	/* 의사 이름 */
+   doctor_name varchar(20) not null, 	/* 의사 이름 */
+   speciality_code int REFERENCES speciality(speciality_code) /* 진료과 코드 */
 );
 
 --테이블 문제 발생 시 삭제
@@ -53,7 +54,7 @@ create table treatment(
     phone varchar(14),   /* 전화번호 */
 
     primary key(treatment_code),
-    foreign key(id) REFERENCES member_tbl(id),
+    foreign key(id) REFERENCES membertbl(id),
     foreign key(speciality_code) REFERENCES speciality(speciality_code),
     foreign key(doctor_code) REFERENCES doctor(doctor_code)
 );
@@ -63,41 +64,3 @@ drop table treatment;
 
 --treatment 테이블 생성 확인
 select * from treatment;
-/*
---예약 테이블 생성
---예약코드 --진료코드  --아이디 --회원이름 --진료과 코드 --의사 코드 --예약 날짜 및 시간 --전화번호
-create table reservation(
-    reservation_code int,   
-    treatment_code int,
-    id varchar(15),
-    name varchar(20) not null, 
-    speciality_code varchar(5) not null, 
-    doctor_code varchar(20) not null, 
-    reservation_date date not null, 
-    phone varchar(14), 
-    
-    primary key(reservation_code, id)
-);
-
---reservation 테이블 생성 확인
-select * from reservation;
-
-*/
-
-/*
---진료대기 테이블 생성
---진료코드 ,진료과 코드, 의사 코드, 아이디, 회원 이름
-create table treatment_waiting(
-	treatment_code varchar(5) primary key,
-   speciality_code int not null, 
-   doctor_code varchar(5), 
-   id varchar(15), 
-   name varchar(20) not null, 
-   foreign key(treatment_code) REFERENCES treatment(treatment_code),
-   foreign key(doctor_code) REFERENCES doctor(doctor_code)
-);
-
-drop table treatment_waiting;
---treatment_waiting 테이블 생성 확인
-select * from treatment_waiting;
-*/
